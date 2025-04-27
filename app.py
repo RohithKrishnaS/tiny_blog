574730cce2876e41d7497f9775620597441d3721
RohithKrishnaS <rohithkrishna1411@gmail.com>
2025-04-27 18:27:13 +0530

    blog with database

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # needed for session management
+app.secret_key = 'supersecretkey'
 
-posts = []  # this will hold our blog posts for now
+# Setup the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
 
-# Dummy user credentials (for now)
+# Dummy user credentials
 USERNAME = 'admin'
-PASSWORD = 'rohith'
+PASSWORD = 'password'
+
+# Define the Post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
 
 # Home Page
 @app.route('/')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 # Create New Post
@@ -23,30 +33,35 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        posts.append({'title': title, 'content': content})
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('create.html')
 
 # Edit Post
-@app.route('/edit/<int:index>', methods=['GET', 'POST'])
-def edit(index):
+@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit(post_id):
     if not session.get('logged_in'):
         flash("Please log in to edit a post!")
         return redirect(url_for('login'))
-    post = posts[index]
+    post = Post.query.get_or_404(post_id)
     if request.method == 'POST':
-        posts[index]['title'] = request.form['title']
-        posts[index]['content'] = request.form['content']
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
         return redirect(url_for('home'))
-    return render_template('edit.html', post=post, index=index)
+    return render_template('edit.html', post=post)
 
 # Delete Post
-@app.route('/delete/<int:index>', methods=['POST'])
-def delete(index):
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
     if not session.get('logged_in'):
         flash("Please log in to delete a post!")
         return redirect(url_for('login'))
-    posts.pop(index)
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
     return redirect(url_for('home'))
 
 # Login
@@ -69,3 +84,7 @@ def logout():
     session.pop('logged_in', None)
     flash("You have been logged out.")
     return redirect(url_for('home'))
+
+# Create database tables if not exist
+with app.app_context():
+    db.create_all()
